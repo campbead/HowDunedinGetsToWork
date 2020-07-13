db8d4beffe82d2b95fa223807bb4bb461b3c86b8
Adam Campbell <campbead@gmail.com>
2020-07-13 18:38:20 +1200

    removed a bunch vestigial code

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(leaflet)
 library(sf)
@@ -213,13 +204,7 @@ ui <- fluidPage(navbarPage(
                 bottom = "auto",
                 width = 350,
                 height = "auto",
-                #h3("How Dunedin gets to work"),
                 h4(selectInput("commuteType", "Transportation Mode", commute_type)),
-                #h4(selectInput("outputType", "Output type", output_type)),
-                #h4(textOutput("name")),
-                #textOutput("percent"),
-                #h4(textOutput("score")),
-                #textOutput("sentence")
             )
         ),
         tags$div(
@@ -312,14 +297,12 @@ ui <- fluidPage(navbarPage(
 server <- function(input, output, session) {
     observe({
         leafletProxy("map") %>% clearPopups()
-        #leafletProxy("map") %>% removeShape("higlih")
         travel_mode = input$commuteType
         event <- input$map_shape_click
         if (is.null(event))
             return()
         isolate({
             showPopup(event$id, event$lat, event$lng)
-            #showTravel(event$id)
             updateMap(event$id, travel_mode)
         })
     })
@@ -383,63 +366,6 @@ server <- function(input, output, session) {
         leafletProxy("map") %>% addPopups(lng, lat, content, layerId = "ID")
     }
 
-    # showTravel <- function(SA_num){
-    #     commuteType_server <- input$commuteType
-    #
-    #     commute_subset <- commute_data %>%
-    #         filter(commute_data$ID_RES == SA_num)
-    #     display <- left_join(SA2, commute_subset, by=c("ID" = "ID_WORK")) %>%
-    #         mutate(highlight_stroke = ifelse(ID == SA_num, 3, 1)) %>%
-    #         mutate(stroke_color = ifelse(ID == SA_num, "black", "grey"))
-    #     home_SA <- SA2 %>%
-    #         filter(SA2$ID == SA_num)
-    #     #display <- left_join(SA2, commute_subset, by="ID_RES")
-    #
-    #     if (commuteType_server == "total") { display$active <- display$Total}
-    #     else if (commuteType_server == "bus"){display$active <- display$Public_bus}
-    #     else if (commuteType_server == "wfh"){display$active <- display$Work_at_home}
-    #     else if (commuteType_server == "DrivePrivate"){display$active <- display$Drive_a_private_car_truck_or_van}
-    #     else if (commuteType_server == "DriveCompany"){display$active <- display$Drive_a_company_car_truck_or_van}
-    #     else if (commuteType_server == "passenger"){display$active <- display$Passenger_in_a_car_truck_van_or_company_bus}
-    #     else if (commuteType_server == "train"){display$active <- display$Train}
-    #     else if (commuteType_server == "bike"){display$active <- display$Bicycle}
-    #     else if (commuteType_server == "walk"){display$active <- display$Walk_or_jog}
-    #     else if (commuteType_server == "ferry"){display$active <- display$Ferry}
-    #     else if (commuteType_server == "other"){display$active <- display$Other}
-    #
-    #     pal <- colorNumeric(
-    #         palette = "Oranges",
-    #         domain = commute_subset$active)
-    #
-    #
-    #     leafletProxy("map") %>%
-    #         addPolygons(data = display,
-    #             color = ~stroke_color,
-    #             fillColor = ~pal(active),
-    #             weight = ~highlight_stroke,
-    #             layerId=~ID,
-    #             smoothFactor = 0.5,
-    #             opacity = 1.0,
-    #             stroke = TRUE,
-    #             fillOpacity = 0.5,
-    #             label = ~ID_NAME,
-    #             highlightOptions = highlightOptions(color = "white",
-    #                 weight = 2,
-    #                 bringToFront = TRUE)) %>%
-    #         addLegend("bottomright",
-    #             data = display,
-    #             pal = pal,
-    #             layerId = 'my_legend',
-    #             values = ~active,
-    #             #title = "foo",
-    #             title =  as.character(tagList(
-    #                 tags$h4("Number of", as.character(home_SA$ID_NAME), " residents working in highlighted areas"),
-    #
-    #             )),
-    #             opacity = 0.5)
-    #
-    # }
-    #
     updateMap <- function(SA_num, travel_mode)
     {
         value_range <- commute_pivot_join %>%filter(direction == "from", mode == travel_mode, type == "diff") %>%select(value) %>% st_drop_geometry()
@@ -523,9 +449,6 @@ server <- function(input, output, session) {
                     textsize = "15px",
                     direction = "auto"),
                 fillOpacity = 0.7
-                # highlightOptions = highlightOptions(color = "black",
-                #     weight = 2,
-                #     bringToFront = FALSE)
             ) %>%
             addLegend_decreasing(
                 position = "bottomright",
@@ -534,118 +457,6 @@ server <- function(input, output, session) {
                 opacity = 0.7,
                 title = my_title
                 )
-    })
-
-
-
-    output$barPlotfrom <- renderPlot({
-
-        event <- input$map_shape_click
-        if (is.null(event))
-            return()
-        SA <-event$id
-        area_name <- SA2 %>% filter(ID == SA) %>% select(ID_NAME)%>%summarise( name = ID_NAME) %>% st_drop_geometry()
-
-        commuting_percent <- rbind(commuting_percent_total, commute_percent(SA,area_name$name))
-        commuting_percent <- commuting_percent%>%
-            mutate(area = factor(area, levels = c("City wide", area_name)))
-
-        ggplot(data = commuting_percent %>% filter(type == "from") , aes(x= colname, y=percent, fill=area)) +
-            geom_col(position = "dodge") +
-            theme_minimal() +
-            ylab("% of work travel\nfrom area") +
-            theme(axis.text.x = element_text(angle = 45, hjust = 1),
-                axis.title.x=element_blank(),
-                legend.position = c(0.6, 0.9),
-                text = element_text(size=20))
-
-    })
-    output$sentence <- renderText({
-        event <- input$map_shape_click
-        trans_mode <- input$commuteType
-        if (is.null(event))
-            return()
-        SA <-event$id
-
-        score <- commute_pivot_join %>%
-            filter(direction == "from", ID == SA, mode == trans_mode, type == 'diff') %>%
-            select(value) %>%
-            st_drop_geometry()
-        sum <- commute_pivot_join %>%
-            filter(direction == "from", ID == SA, mode == trans_mode, type == 'sum') %>%
-            select(value) %>%
-            st_drop_geometry()
-        average <- commute_pivot_join %>%
-            filter(direction == "from", ID == SA, mode == trans_mode, type == 'ifaverage') %>%
-            select(value) %>%
-            st_drop_geometry()
-        pct <- commute_pivot_join %>%
-            filter(direction == "from", ID == SA, mode == trans_mode, type == 'pct') %>%
-            select(value) %>%
-            st_drop_geometry()
-        name <- commute_pivot_join %>%
-            filter(direction == "from", ID == SA, mode == trans_mode, type == 'pct') %>%
-            select(ID_NAME) %>%
-            st_drop_geometry()
-
-        if (trans_mode == "wfh"){
-            sentence <- paste("The ",
-                Score_title(trans_mode), " of ", round(score), " means that ", sum, " people living here typically ",
-                " work at home, compared to a city wide average of ", round(average), " for an area with this many commuters.", sep ="")
-        }
-        else {
-            sentence <- paste("The ",
-                Score_title(trans_mode), " of ", round(score), " means that ", sum, " people living here typically ", mode_verb_2(trans_mode),
-                " to work, compared to a city wide average of ", round(average), " for an area with this many commuters.", sep ="")
-
-        }
-
-        return(sentence)
-    })
-
-    output$name <- renderText({
-        event <- input$map_shape_click
-        if (is.null(event))
-            return()
-        SA <-event$id
-        name <- commute_pivot_join %>%
-            filter(direction == "from", ID == SA, mode == input$commuteType) %>%
-            select(ID_NAME) %>%
-            st_drop_geometry() %>%
-            unique()
-        return(paste(name))
-    })
-
-    output$score <- renderText({
-        event <- input$map_shape_click
-        trans_mode <- input$commuteType
-        if (is.null(event))
-            return()
-        SA <-event$id
-        score <- commute_pivot_join %>%
-            filter(direction == "from", ID == SA, mode == trans_mode, type == 'diff') %>%
-            select(value) %>%
-            st_drop_geometry()
-        return(paste(Score_title(trans_mode), ": ", round(score)))
-    })
-
-    output$percent <- renderText({
-        event <- input$map_shape_click
-        if (is.null(event))
-            return()
-        SA <-event$id
-        trans_mode <- input$commuteType
-        pct <- commute_pivot_join %>%
-            filter(direction == "from", ID == SA, mode == input$commuteType, type == 'pct') %>%
-            select(value) %>%
-            st_drop_geometry()
-        if (trans_mode == "wfh") {
-            sentence = paste(round(pct*100, digits = 1), "% of workers living here typically work at home.", sep ="")
-        }
-        else {
-            sentence = paste(round(pct*100, digits = 1), "% of workers living here typically get to work by ", mode_verb(trans_mode),".", sep ="")
-        }
-        return(sentence)
     })
 }
 
